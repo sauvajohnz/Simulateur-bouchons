853ec973271dbbdf5297b92f8b616dfb193088c8
Mathieu G <35891922+sauvajohnz@users.noreply.github.com>
2022-05-26 14:50:49 +0200

    Optimisation
    Optimisation de l'ajout de boutons en mettant l'affichage des boutons directement dans le fichier de leur class

--- boutoncoulissant.py
+++ boutoncoulissant.py
@@ -3,8 +3,12 @@ import pygame
 #y€[150,180] cad 30
 
 class BoutonCoulissant(pygame.sprite.Sprite):
-    def __init__(self, pos_y, valeur_max, valeur_init, valeur_min = 1,):
+    def __init__(self, pos_y, valeur_max, valeur_init, valeur_min = 1,fond = "null"):
         super().__init__()
+        if fond != "null":
+            self.fond = fond
+        else:
+            self.fond = fond
         self.y = pos_y
         self.valeur_min = valeur_min
         self.valeur_max = valeur_max
@@ -18,7 +22,25 @@ class BoutonCoulissant(pygame.sprite.Sprite):
         self.rect.center = [x, self.y]
 
     def valeur(self):
-        valeur = ((self.rect[0] - 960)*self.valeur_max/310) + self.valeur_min #Règle de trois, permettant de renvoyer la valeur du bouton
+        if self.valeur_min != 1:
+            valeur = ((self.rect[0] - 960)*self.valeur_max/310) + self.valeur_min #Règle de trois, permettant de renvoyer la valeur du bouton
+        else:
+            valeur = ((self.rect[0] - 960) * self.valeur_max / 310)
         if valeur > 0 and str(valeur) != 'None':
             return valeur
         return 0
+
+    def update(self, screen, text):
+        fontText = pygame.font.Font('freesansbold.ttf', 15)
+        text = str(text)
+        # Carré blanc, avec ou sans fond
+        if self.fond != "null":
+            screen.blit(pygame.image.load(self.fond), (960, self.y - 14.5))
+        else:
+            pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(960, self.y - 14.5, 310, 30))
+        # Text affiché au dessus du carré
+        text = fontText.render(text, True, (255, 255, 255), (96, 96, 96))
+        textRect = text.get_rect()
+        textRect.center = (1120, self.y - 25.5)
+        screen.blit(text, textRect)
+
--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from boutoncoulissant import BoutonCoulissant
 from boutoncliquant import BoutonCliquant
 from math import pow
 pygame.init()
-#To do:
-# - Optimiser l'espace de rajout des options en mettant le code dans les class
 
 
 ###Mise en forme globale de l'application###
@@ -16,6 +14,7 @@ pygame.display.set_caption('Simulateur bouchons')
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
+acceleration = 1
 
 
 ####### Mise en forme du texte dans la section options ###
@@ -51,16 +50,18 @@ placer_vehicules(voiture_init, diametre_init)
 
 #Les boutons
 boutons_group = pygame.sprite.Group()
-boutoncoulissant1 = BoutonCoulissant(164.5, 40, 30) #Vitesse globale demandee
+boutoncoulissant1 = BoutonCoulissant(164.5, 40, 30, fond = "images/fond_vitesse_globale.png") #Vitesse globale demandee
 boutoncoulissant2 = BoutonCoulissant(219.5, 100, voiture_init) #Densité vehicules
-boutoncoulissant3 = BoutonCoulissant(329.5, 40, 30) #Vitesse vehicule genant
+boutoncoulissant3 = BoutonCoulissant(329.5, 40, 30, fond = "images/fond_vitesse_globale.png") #Vitesse vehicule genant
 boutoncliquant1 = BoutonCliquant(274.5, False) #Activer vehicule genant
 boutoncoulissant4 = BoutonCoulissant(385.5, 180, diametre_init, valeur_min=10) #Diametre rond point
+boutoncoulissant5 = BoutonCoulissant(445.5, 400, 104.516) #Acceleration
 boutons_group.add(boutoncoulissant1)
 boutons_group.add(boutoncoulissant2)
 boutons_group.add(boutoncoulissant3)
 boutons_group.add(boutoncliquant1)
 boutons_group.add(boutoncoulissant4)
+boutons_group.add(boutoncoulissant5)
 #############################################################
 
 def adapter_vitesse_voiture_genante():
@@ -117,45 +118,20 @@ while 1:
     pygame.draw.arc(screen, (0, 0, 0), (150, 80, 650, 643), 0, 360)
     ################################
 
-    #Bouton vitesse globale#
-    screen.blit(pygame.image.load("images/fond_vitesse_globale.png"), (960,150))
-    textVitesseGlobal = fontText.render(f"Vitesse globale demandée({round(boutoncoulissant1.valeur())} km/h)", True, (255, 255, 255), (96, 96, 96))
-    textRect3 = textVitesseGlobal.get_rect()
-    textRect3.center = (1120, 140)
-    screen.blit(textVitesseGlobal, textRect3)
-    #########################
+
 
     #Bouton densité vehicules#
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(960, 205, 310, 30, width=300))
-    textVehiculeGlobal = fontText.render(f"Densité de vehicules({nombre_voitures_actuel} vehicules)", True, (255, 255, 255), (96, 96, 96))
-    textRect4 = textVehiculeGlobal.get_rect()
-    textRect4.center = (1120, 195)
-    screen.blit(textVehiculeGlobal, textRect4)
-    ########################
-
-    #Bouton activer vebicule genant#
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(960, 260, 32, 30))
     textVehiculeGenant = fontText.render(f"Activer vehicule genant", True,(255, 255, 255), (96, 96, 96))
     textRect5 = textVehiculeGenant.get_rect()
     textRect5.center = (1120, 275)
     screen.blit(textVehiculeGenant, textRect5)
     ########################
 
-    #Bouton vitesse vehicule genant#
-    screen.blit(pygame.image.load("images/fond_vitesse_globale.png"), (960,315))
-    textVitesseVehiculeGenant = fontText.render(f"Vitesse du vehicule genant({round(boutoncoulissant3.valeur())} km/h)", True, (255, 255, 255), (96, 96, 96))
-    textRect6 = textVitesseVehiculeGenant.get_rect()
-    textRect6.center = (1120, 305)
-    screen.blit(textVitesseVehiculeGenant, textRect6)
-    ########################
-
-    #Bouton diametre rond point#
-    pygame.draw.rect(screen, (255, 255, 255), pygame.Rect(960, 370, 310, 30, width=300))
-    textDiametreRd = fontText.render(f"Diametre rond point({round(boutoncoulissant4.valeur())} m)", True,(255, 255, 255), (96, 96, 96))
-    textRect7 = textDiametreRd.get_rect()
-    textRect7.center = (1120, 360)
-    screen.blit(textDiametreRd, textRect7)
-    ########################
+    boutoncoulissant1.update(screen, f"Vitesse globale demandée({round(boutoncoulissant1.valeur())} km/h)")
+    boutoncoulissant2.update(screen, f"Densité de vehicules({nombre_voitures_actuel} vehicules)")
+    boutoncoulissant3.update(screen, f"Vitesse du vehicule genant({round(boutoncoulissant3.valeur())} km/h)")
+    boutoncoulissant4.update(screen, f"Diametre rond point({round(boutoncoulissant4.valeur())} m)")
+    boutoncoulissant5.update(screen, f"Accélerer ({round(boutoncoulissant5.valeur())}%)")
 
 
     ###Ajout des sprites###
@@ -189,12 +165,12 @@ while 1:
     textVitesseMoyenne = fontText.render(f"Vitesse moyenne: {round(vitesse_totale/nombre_voitures_actuel)}km/h", True, (255, 255, 255), (96, 96, 96))
     screen.blit(textVitesseMoyenne, (0,0))
     #######################
-    print(nombre_voitures_actuel)
 
     #####Text Remplissage#####
     textRemplMoyen = fontText.render(f"Remplissage: {round(boutoncoulissant2.valeur())}%", True, (255, 255, 255), (96, 96, 96))
     screen.blit(textRemplMoyen, (0, 14))
     ##########################
+
 
     pygame.display.flip()
 
